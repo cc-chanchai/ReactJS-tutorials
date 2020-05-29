a7a75e6f11b3e783dc63b76ec588bd8ff1f66f4e
chanchai9797 <chanchai9797@gmail.com>
2020-05-29 18:30:06 +0700

    unit2 component

--- unit2 - component and JSX/component/src/App.js
+++ unit2 - component and JSX/component/src/App.js
@@ -1,23 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Post from './Post'
 
 function App() {
+  const name = 'cc-chanchai'
+  const num1 = 10
+  const num2 = 15
+  const randomNumber = Math.random()
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <h3>
+          cc-chanchai
+        </h3>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My name's : { name }
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {num1} + {num2} ได้ผลลัพธ์ คือ { num1 + num2}
+        </p>
+        <p>
+          random number > 0.5 : {randomNumber > 0.5 ? "Yes" : "No"}
+        </p>
+        <Post/>
       </header>
     </div>
   );
---
+++ unit2 - component and JSX/component/src/Post.js
@@ -0,0 +1,11 @@
+import React , { component, Component } from 'react'
+class Post extends Component {
+    render() {
+        return (
+            <div>
+                This Post component!!!
+            </div>
+        )
+    }
+}
+export default Post
