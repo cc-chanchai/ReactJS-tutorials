cc7e36992c6c85caa0ffc24418c403c9bcee64c2
chanchai9797 <chanchai9797@gmail.com>
2020-05-29 19:19:58 +0700

    component - end unit2

---
+++ unit2 - component and JSX/component/src/Child1.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Child1 = () => {
+    return (
+        <div>
+            <h1>content from Child1</h1>
+        </div>
+    )
+}
+export default Child1
---
+++ unit2 - component and JSX/component/src/Comment.js
@@ -0,0 +1,18 @@
+import React from 'react'
+// Not Recommend 
+// function Comment() {
+//     return (
+//         <div>
+//             This Comment : Function Component
+//         </div>
+//     )
+// }
+// Recommend
+const Comment = () => {
+    return (
+                <div>
+                    This Comment : Function Component
+                </div>
+            )
+}
+export default Comment
